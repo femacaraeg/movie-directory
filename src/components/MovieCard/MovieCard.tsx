33d0e71a7e073d946aef004043c7f9046e6bc99b
Fe Macaraeg <fem@codev.com>
2022-08-14 23:27:40 +0800

    Install and setup material ui.

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+
 interface Movie {
   title: string;
   year?: string;
@@ -10,7 +12,7 @@ export default function MovieCard(props: Movie) {
   const { title, year, poster } = props;
 
   return (
-    <div className="rounded-lg w-80 cursor-pointer hover:bg-gray-700">
+    <div className="rounded-lg w-80 hover:bg-gray-700">
       <div className="rounded-t-lg">
         <img
           src={poster}
@@ -18,13 +20,19 @@ export default function MovieCard(props: Movie) {
           className="object-cove w-full h-1/2 rounded-t-lg"
         />
       </div>
-      <div className="gap-1 p-2 flex  rounded-b-lg h-14">
+      <div className="gap-1 p-2 flex rounded-b-lg h-14 justify-between">
         <div className="w-3/4">
           <p className="text-sm text-white">
             <span className="font-bold">{title}</span>
             &nbsp;({year})
           </p>
         </div>
+        <button>
+          <AddCircleIcon
+            sx={{ color: "white" }}
+            className="hover: cursor-pointer"
+          />
+        </button>
       </div>
     </div>
   );

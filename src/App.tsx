207d4e0e52e585f50e4ccccf7f33cf5a7946e531
Fe Macaraeg <fem@codev.com>
2022-09-15 14:08:10 +0800

    Use HashRouter.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import AppBar from "./components/AppBar";
 
